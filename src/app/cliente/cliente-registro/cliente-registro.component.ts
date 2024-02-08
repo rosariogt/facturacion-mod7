139245b64b33ae87f4d4470b36539c57dae9691b
Nicolas Colque <nicolascon93@gmail.com>
2024-02-08 18:49:42 -0400

    Registro de formulario cliente mas validaciones

--- src/app/cliente/cliente-registro/cliente-registro.component.ts
+++ src/app/cliente/cliente-registro/cliente-registro.component.ts
@@ -10,28 +10,17 @@ import { Cliente } from 'src/app/models/cliente';
 export class ClienteRegistroComponent implements OnInit {
 
   registroForm: FormGroup;
-  /* cliente? : Cliente = {
-    codigo: 'RGT',
-    nombre: 'Nombre',
-    numeroDocumento: '9115425',
-    tipoDocumento: 1,
-    correo: 'rgt@gmail.com'
-  } */
   private emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   formRespuesta = '';
 
+
   constructor(private fb: FormBuilder) {
-    /* this.registroForm = new FormGroup({
-      nombre: new FormControl('', Validators.required),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
-    }); */
     this.registroForm = this.fb.group({
-      codigo: ['', Validators.required],
-      nombre: ['', Validators.required],
-      numeroDocumento: ['', Validators.required, Validators.minLength(7)],
-      tipoDocumento: ['', Validators.required],
-      correo: ['', Validators.required, Validators.email, Validators.pattern(this.emailPattern)],
+      codigo: ['', [Validators.required, Validators.minLength(6)]],
+      nombre: ['', [Validators.required, Validators.minLength(2)]],
+      numeroDocumento: ['', [Validators.required, Validators.minLength(7)]],
+      tipoDocumento: ['', [Validators.required]],
+      correo: ['', [Validators.required, Validators.email, Validators.pattern(this.emailPattern)]],
     });
    }
 
@@ -41,23 +30,11 @@ export class ClienteRegistroComponent implements OnInit {
   registrarCliente() {
     /* console.log('Cliente registro', this.cliente); */
     if (this.registroForm.valid) {
-      this.formRespuesta = 'Formulario VALIDO';
+      this.formRespuesta = 'FORMULARIO VALIDO';
 
     } else {
-      this.formRespuesta = 'Formulario NO VALIDO';
+      this.formRespuesta = 'Formulario no valido';
     }
   }
 
-  get codigo(){
-    return this.registroForm.get('codigo');
-  }
-  get nombre(){
-    return this.registroForm.get('nombre');
-  }
-  get numeroDocumento(){
-    return this.registroForm.get('numeroDocumento');
-  }
-  get correo(){
-    return this.registroForm.get('correo');
-  }
 }
